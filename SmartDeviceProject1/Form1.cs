dc24b51d59823f8b46ed6b303db1881ef325b41a
SpaceyPuppy <chris.phelan@me.com>
2017-07-09 14:58:27 +1000

    Moved prototype project to Git repo

---
+++ SmartDeviceProject1/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SmartDeviceProject1
+{
+    public partial class homeForm : Form
+    {
+        public homeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void inputPanel1_EnabledChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
